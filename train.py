0d29e7fcab062d5fdaeeb3ecc315b8cad2eafd17
Drew FINNIS <dfinnis@e2r4p23.42.fr>
2019-10-04 16:25:54 +0200

    moved old files to archive

--- train.py
+++ train.py
@@ -7,7 +7,12 @@ def feature_normalize(X):
     mu = np.mean(X)
     sigma = np.std(X)
     normalized_features = (X-mu)/sigma if sigma != 0 else X
-    return normalized_features
+    return normalized_features, mu, sigma
+
+# def denormalize(theta, X_mu, X_sigma, Y_mu, Y_sigma):
+#     theta[0] = theta[0] * Y_sigma + Y_mu
+#     theta[1] = theta[1] * X_sigma + X_mu
+#     return theta
 
 def predict(X, theta):
     return theta[0] + (theta[1] * X)
@@ -36,7 +41,7 @@ def visualize_regression(theta, X_norm, X, y):
 
     x_max = np.max(X) + 10000
     x_min = np.min(X) - 10000
-    ax.set_xlim([x_min, x_max])
+    ax.set_xlim([0, x_max])
     ax.set_ylim(np.min(y) - 1000, np.max(y) + 1000)
     ax.scatter(X, y, color='blue')
     
@@ -71,7 +76,12 @@ def main():
     data = pd.read_csv("data.csv")
     X = np.array(data['km'], dtype='float64')
     y = np.array(data['price'], dtype='float64')
-    X_norm = feature_normalize(X)
+    X_norm, X_mu, X_sigma = feature_normalize(X)
+    # Y_norm, Y_mu, Y_sigma = feature_normalize(y)
+    # print("X_mu: {}" .format(X_mu))
+    # print("X_sigma: {}" .format(X_sigma))
+    # print("Y_mu: {}" .format(Y_mu))
+    # print("Y_sigma: {}" .format(Y_sigma))
 
     theta = np.array([0, 0], dtype='float64')
     theta, J_history = fit(X_norm, y, theta, 0.01, 1500)
@@ -79,6 +89,7 @@ def main():
     visualize_cost(J_history)
     visualize_regression(theta, X_norm, X, y)
 
+    # theta = denormalize(theta, X_mu, X_sigma, Y_mu, Y_sigma)
     save_theta(theta)
     
 
